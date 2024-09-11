6f243a66984f4b062dd2700d25d21f60cd4c819c
AnthonyAriass <anthonyarias2000@estudiantec.cr>
2024-09-10 19:17:26 -0600

    basic structure

--- backend/app/app.py
+++ backend/app/app.py
@@ -2,6 +2,26 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+def connect_postgres():
+
+    return
+
+def connect_mysql():
+
+    return
+
+def connect_sqlserver():
+    
+    return
+
+
 @app.get("/diagram")
-def get_diagram():
-    return {"www.foto.com"}
+def get_diagram(data):
+    if data["bd"] == "postgres":
+        connect_postgres()
+    elif data["bd"] == "mysql":
+        connect_mysql()
+    else:
+        connect_sqlserver()
+
+    return {"url": "www.foto.com"}
