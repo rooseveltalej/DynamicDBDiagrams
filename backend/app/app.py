49e90ac733abe97ba5495d8812a77a5ffcaca8e7
Luis <lmendez@estudiantec.cr>
2024-09-14 11:33:55 -0600

    query schema, format schema, image and url image generate

--- backend/app/app.py
+++ backend/app/app.py
@@ -13,26 +13,76 @@ def is_running():
     return {"status": "running"}
 
 #Se reciben los parámetros mediante una solicitud HTTP con los argumentos, y luego se arma el diccionario data
+
 @app.get("/diagram")
-def get_diagram(
-    bd: str = Query(...),
-    host: str = Query(...),
-    port: int = Query(...),
-    user: str = Query(...),
-    password: str = Query(...),
-    database: str = Query(...)
-):
-    data = {
-        "bd": bd,
-        "host": host,
-        "port": port,
-        "user": user,
-        "password": password,
-        "database": database
-    }
-    
+
+def get_diagram():
+    # Parametros para solicitud en MySQL
+    """"
+    dataBase = "proyecto"
+    hosteador = "localhost"
+    puerto = 3306
+    usuario = "root"
+    contrasena = ""
+    baseDatos = "proyecto"
+    """
+    # Parametros para solicitud en PostgreSQL
+    """"dataBase = "proyecto"
+    hosteador = "COMPUTER"
+    puerto = 4096
+    usuario = "postgres"
+    contrasena = "Windows Authentication"
+    baseDatos = "proyecto" """
+    # Parametros para solicitud en SQL Server
+    """driver = "SQL Server"
+    server = "localhost"
+    database = "proyecto"
+    uid = "sa"  # Tu nombre de usuario
+    pwd = "123"  # Tu contraseña"""
+    # Data enviada a la función conexión para SQL SERVER
+    """dataSqlServer = {
+        "driver": driver,
+        "server": server,
+        "database": database,
+        "uid": uid,
+        "pwd": pwd
+    }"""
+
+    # Data enviada a la función conexión para MySQL y PostgreSQL
+    """data = {
+        "bd": dataBase,
+        "host": hosteador,
+        "port": puerto,
+        "user": usuario,
+        "password": contrasena,
+        "database": baseDatos
+    }"""
     #El information schema de cualquier de las 3 BD
     schema = None
+
+    #Formatear el esquema en PlantUML
+    #Consulta MySQL
+    """schema = connect_mysql(data)
+    formatted_schema = format_schema(schema)
+    encode_text = encode_plantuml(formatted_schema)
+    generate_uml_image(encode_text)
+    # Obtener el enlace de la imagen generada por PlantUML
+    uml_url = generate_plantuml_image(formatted_schema)"""
+    #Consulta PostgreSQL
+    """schema = connect_postgres(data)
+    formatted_schema = format_schema(schema)
+    encode_text = encode_plantuml(formatted_schema)
+    generate_uml_image(encode_text)
+    uml_url = generate_plantuml_image(formatted_schema)"""
+    #Consulta SQL Server
+    """schema = connect_sqlserver(dataSqlServer)
+    formatted_schema = format_schema(schema)
+    encode_text = encode_plantuml(formatted_schema)
+    generate_uml_image(encode_text)
+    uml_url = generate_plantuml_image(formatted_schema)"""
+    uml_url = None
+    return {"url": uml_url}
+
 
     try:
         # Conectar a la base de datos según el tipo
@@ -47,10 +97,14 @@ def get_diagram(
         
         # Formatear el esquema en PlantUML
         formatted_schema = format_schema(schema)
+        encode_text = encode_plantuml(formatted_schema)
+        generate_uml_image(encode_text)
         
         # Obtener el enlace de la imagen generada por PlantUML
         uml_url = generate_plantuml_image(formatted_schema)
         
+
+
         return {"url": uml_url}
 
     except HTTPException as e:
@@ -86,18 +140,15 @@ def connect_postgres(data):
                     information_schema.table_constraints tc
                     ON c.table_schema = tc.table_schema
                     AND c.table_name = tc.table_name
-                    AND c.column_name = (
-                        SELECT
-                            kcu.column_name
-                        FROM
-                            information_schema.key_column_usage kcu
-                        WHERE
-                            kcu.constraint_name = tc.constraint_name
-                            AND kcu.table_schema = tc.table_schema
-                            AND kcu.table_name = tc.table_name
-                    )
+                LEFT JOIN
+                    information_schema.key_column_usage kcu
+                    ON kcu.table_schema = tc.table_schema
+                    AND kcu.table_name = tc.table_name
+                    AND kcu.column_name = c.column_name
+                    AND kcu.constraint_name = tc.constraint_name
                 WHERE
-                    c.table_schema = %s
+                    c.table_schema = 'public'
+                    AND tc.constraint_type = 'PRIMARY KEY'
             ),
             foreign_keys AS (
                 SELECT
@@ -116,7 +167,7 @@ def connect_postgres(data):
                     ON tc.constraint_name = ccu.constraint_name
                 WHERE
                     tc.constraint_type = 'FOREIGN KEY'
-                    AND tc.table_schema = %s
+                    AND tc.table_schema = 'public'
             )
             SELECT
                 ti.table_schema,
@@ -138,6 +189,7 @@ def connect_postgres(data):
                 ti.table_schema,
                 ti.table_name,
                 ti.column_name;
+
         """, (data["database"], data["database"]))
         schema = cur.fetchall()
         cur.close()
@@ -158,58 +210,40 @@ def connect_mysql(data):
         )
         cur = conn.cursor()
         cur.execute("""
-            WITH table_info AS (
-                SELECT
-                    table_schema,
-                    table_name,
-                    column_name,
-                    data_type,
-                    CASE
-                        WHEN column_key = 'PRI' THEN 'PK'
-                        ELSE ''
-                    END AS constraint_type
-                FROM
-                    information_schema.columns
-                WHERE
-                    table_schema = %s
-            ),
-            foreign_keys AS (
-                SELECT
-                    k.table_schema AS source_schema,
-                    k.table_name AS source_table,
-                    k.column_name AS source_column,
-                    c.table_name AS target_table,
-                    c.column_name AS target_column
-                FROM
-                    information_schema.key_column_usage k
-                JOIN
-                    information_schema.constraint_column_usage c
-                    ON k.constraint_name = c.constraint_name
-                WHERE
-                    k.referenced_table_name IS NOT NULL
-                    AND k.table_schema = %s
-            )
-            SELECT
-                ti.table_schema,
-                ti.table_name,
-                ti.column_name,
-                ti.data_type,
-                ti.constraint_type AS pk_status,
-                fk.source_table AS fk_source_table,
-                fk.source_column AS fk_source_column,
-                fk.target_table AS fk_target_table,
-                fk.target_column AS fk_target_column
-            FROM
-                table_info ti
-            LEFT JOIN
-                foreign_keys fk
-                ON ti.table_name = fk.source_table
-                AND ti.column_name = fk.source_column
-            ORDER BY
-                ti.table_schema,
-                ti.table_name,
-                ti.column_name;
-        """, (data["database"], data["database"]))
+            SELECT 
+                NULL AS 'Tabla', -- Placeholder para cumplir con el índice de la función
+                c.table_name AS 'Tabla',
+                c.column_name AS 'Columna',
+                c.data_type AS 'Tipo de Dato',
+                CASE 
+                    WHEN kcu.constraint_name IS NOT NULL AND tc.constraint_type = 'PRIMARY KEY' THEN 'PK'
+                    ELSE NULL
+                END AS 'Es PK',
+                CASE 
+                    WHEN tc.constraint_type = 'FOREIGN KEY' THEN c.table_name
+                    ELSE NULL
+                END AS 'Tabla Origen FK',
+                CASE 
+                    WHEN tc.constraint_type = 'FOREIGN KEY' THEN c.column_name
+                    ELSE NULL
+                END AS 'Columna Origen FK',
+                kcu.referenced_table_name AS 'Tabla Objetivo FK',
+                kcu.referenced_column_name AS 'Columna Objetivo FK'
+            FROM 
+                information_schema.columns c
+                LEFT JOIN information_schema.key_column_usage kcu 
+                    ON c.table_name = kcu.table_name 
+                    AND c.column_name = kcu.column_name 
+                    AND c.table_schema = kcu.table_schema
+                LEFT JOIN information_schema.table_constraints tc
+                    ON tc.constraint_name = kcu.constraint_name
+                    AND tc.table_schema = c.table_schema
+                    AND tc.table_name = c.table_name
+            WHERE 
+                c.table_schema = %s
+            ORDER BY 
+                c.table_name, c.ordinal_position;
+        """, (data["database"],))  # Pasar el parámetro como una tupla
         schema = cur.fetchall()
         cur.close()
         conn.close()
@@ -221,17 +255,20 @@ def connect_mysql(data):
 def connect_sqlserver(data):
     try:
         conn = pyodbc.connect(
-            f"DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={data['host']},{data['port']};DATABASE={data['database']};UID={data['user']};PWD={data['password']}"
-        )
+            driver=data["driver"],
+            server=data["server"],
+            database=data["database"],
+            uid=data["uid"],
+            pwd=data["pwd"])
         cur = conn.cursor()
         cur.execute("""
-            WITH table_info AS (
+            WITH TableInfo AS (
                 SELECT
-                    t.TABLE_SCHEMA,
-                    t.TABLE_NAME,
+                    c.TABLE_SCHEMA,
+                    c.TABLE_NAME,
                     c.COLUMN_NAME,
                     c.DATA_TYPE,
-                    CASE
+                    CASE 
                         WHEN pk.COLUMN_NAME IS NOT NULL THEN 'PK'
                         ELSE ''
                     END AS constraint_type
@@ -245,12 +282,15 @@ def connect_sqlserver(data):
                     INFORMATION_SCHEMA.KEY_COLUMN_USAGE pk
                     ON c.TABLE_NAME = pk.TABLE_NAME
                     AND c.COLUMN_NAME = pk.COLUMN_NAME
-                    AND pk.CONSTRAINT_NAME LIKE 'PK%'
+                    AND pk.CONSTRAINT_NAME IN (
+                        SELECT CONSTRAINT_NAME 
+                        FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS 
+                        WHERE CONSTRAINT_TYPE = 'PRIMARY KEY'
+                    )
                 WHERE
                     t.TABLE_TYPE = 'BASE TABLE'
-                    AND c.TABLE_SCHEMA = 'dbo'
             ),
-            foreign_keys AS (
+            ForeignKeys AS (
                 SELECT
                     fk.TABLE_SCHEMA AS source_schema,
                     fk.TABLE_NAME AS source_table,
@@ -267,25 +307,35 @@ def connect_sqlserver(data):
                     ON rc.UNIQUE_CONSTRAINT_NAME = ccu.CONSTRAINT_NAME
             )
             SELECT
-                ti.table_schema,
-                ti.table_name,
-                ti.column_name,
-                ti.data_type,
-                ti.constraint_type AS pk_status,
-                fk.source_table AS fk_source_table,
-                fk.source_column AS fk_source_column,
-                fk.target_table AS fk_target_table,
-                fk.target_column AS fk_target_column
+                ti.TABLE_SCHEMA AS 'Esquema',
+                ti.TABLE_NAME AS 'Tabla',
+                ti.COLUMN_NAME AS 'Columna',
+                ti.DATA_TYPE AS 'Tipo de Dato',
+                CASE
+                    WHEN ti.constraint_type = 'PK' THEN 'PK'
+                    ELSE NULL
+                END AS 'Es PK',
+                CASE
+                    WHEN fk.source_table IS NOT NULL THEN fk.source_table
+                    ELSE NULL
+                END AS 'Tabla Origen FK',
+                CASE
+                    WHEN fk.source_column IS NOT NULL THEN fk.source_column
+                    ELSE NULL
+                END AS 'Columna Origen FK',
+                fk.target_table AS 'Tabla Objetivo FK',
+                fk.target_column AS 'Columna Objetivo FK'
             FROM
-                table_info ti
+                TableInfo ti
             LEFT JOIN
-                foreign_keys fk
-                ON ti.table_name = fk.source_table
-                AND ti.column_name = fk.source_column
+                ForeignKeys fk
+                ON ti.TABLE_NAME = fk.source_table
+                AND ti.COLUMN_NAME = fk.source_column
             ORDER BY
-                ti.table_schema,
-                ti.table_name,
-                ti.column_name;
+                ti.TABLE_SCHEMA,
+                ti.TABLE_NAME,
+                ti.COLUMN_NAME;
+
         """)
         schema = cur.fetchall()
         cur.close()
@@ -352,3 +402,39 @@ def generate_plantuml_image(uml_code):
             raise Exception(f"Error al generar la imagen: {response.status_code}")
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error al generar imagen de PlantUML: {str(e)}")
+
+
+def encode_plantuml(text):
+    # Remove the zlib header (first two bytes) and the checksum (last four bytes)
+    zlibbed_str = zlib.compress(text.encode('utf-8'))
+    compressed_string = zlibbed_str[2:-4]
+    
+    # Encode to base64
+    encoded = base64.b64encode(compressed_string)
+    
+    # Translate base64 to PlantUML's encoding alphabet
+    plantuml_alphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_'
+    base64_alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+    translation_table = str.maketrans(base64_alphabet, plantuml_alphabet)
+    
+    # Return the translated string
+    return encoded.decode('utf-8').translate(translation_table)
+
+
+def generate_uml_image(encoded_text):
+    # Base URL for PlantUML server
+    base_url = "http://www.plantuml.com/plantuml/png/"
+    
+    # Full URL with encoded UML text
+    full_url = base_url + encoded_text
+    
+    # Make the request
+    response = requests.get(full_url)
+    
+    # Save the resulting image
+    if response.status_code == 200:
+        with open("uml_diagram.png", "wb") as file:
+            file.write(response.content)
+        print("Image generated and saved as uml_diagram.png")
+    else:
+        print(f"Error generating diagram: {response.status_code}")
